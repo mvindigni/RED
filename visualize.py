4eb3adab9acad0b36aca689d09e418cd5509a831
mvindigni <42945905+mvindigni@users.noreply.github.com>
2021-11-04 18:48:35 -0400

    added old data functionality

--- visualize.py
+++ visualize.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import matplotlib as plt
+from datetime import datetime
+import os
 
 def main():
+    # read datetime
+    now = datetime.now()
+    date = now.strftime("%D").replace("/", "-")
+    
     # read csv data into pandas dataframe
     df = pd.read_csv('data.csv')
     df['time'] = pd.to_datetime(df['time'])
@@ -16,9 +22,10 @@ def main():
     names = ["temp", "sig", "battery"]
 
     # save each plot as a .png in our folder
+    os.mkdir("graphs/" + date + "/")
     for i in range(3):
         fig = plots[i].get_figure()
-        fig.savefig("graphs/" + names[i] + ".png")
+        fig.savefig("graphs/" + date + "/" + names[i] + ".png")
 
 if __name__ == "__main__":
     main()
